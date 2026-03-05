06bd78f254b4780b31b1b596a50cca5e9480850d
Tracy Mutoni <tracy.n.mutoni@gmail.com>
2026-03-05 12:48:46 -0500

    docs: add PRD

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/landing-v3',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
